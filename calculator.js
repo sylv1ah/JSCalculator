3391068c49b2c7014c0797727f7c246a810c8387
seabasshoang <sylviahoang@hotmail.co.uk>
2019-02-21 18:25:17 +0000

    disabled calculator from beginning with multiple zeros

--- calculator.js
+++ calculator.js
@@ -1,57 +1,72 @@
-var currentVal = [""];
-var firstInput = 0;
+var currentDisplay = 0;
+var storedInput = 0;
 var secondInput =0;
 var operator = ""
 var runningTotal = "";
 var finalResult = "";
 
+// const keys = document.querySelector('.keys');
+// const calculator = document.querySelector('.calculator');
+
+// keys.addEventListener('click', e => {
+//     if(e.target.matches('button')){
+        
+//     }
+// })
+
 function inputNum(num){
-    
+    if(document.getElementById('display').value === '0'){
+        document.getElementById('display').value = num;
+        currentDisplay = document.getElementById('display').value;
+    }else{
         document.getElementById('display').value = document.getElementById('display').value + num;
-    //  document.getElementById('display').value = currentVal;
+        currentDisplay = document.getElementById('display').value;
+    }
     
-    // console.log("current val = " + currentVal);
-    // console.log("operator = " + operator);
-    // console.log("firstInput = " + firstInput);
-    // console.log("secondInput = " + secondInput)
+    console.log("currentDisplay = " + currentDisplay);
+    
 };
 
 function inputSymbol(sym){
     document.getElementById('display').value = document.getElementById('display').value + sym;
     // firstInput = document.getElementById('display').value.slice(0,-1);
-    // operator = sym;
-    // console.log("currentVal = " + currentVal);
-    // console.log("operator = " + operator);
-    // console.log("firstInput = " + firstInput);
-    // console.log("secondInput = " + secondInput)
+    
+    operator = sym;
+    console.log("operator = " + operator)
+    
 };
 
 function clearDisplay(){
-    document.getElementById('display').value = "";
-    // currentVal = "";
-    // firstInput = "";
-    // secondInput = "";
-    // operator = "";
-    // runningTotal = "";
-    // finalResult = "";
-    // console.log("currentVal = " + currentVal);
-    // console.log("operator = " + operator);
-    // console.log("firstInput = " + firstInput);
-    // console.log("secondInput = " + secondInput)
+    document.getElementById('display').value = "0";
+    currentDisplay = "0";
+    
+    console.log("currentDisplay = " + currentDisplay);
+    
 };
 
 function equate(){
     var equation = document.getElementById('display').value;
     if(equation){
         document.getElementById('display').value = eval(equation);
-        currentVal = 0;
+        current = 0;
 
     }
     console.log("equation = " + equation)
-}
+};
 
-// function decimal(){
-//     if(!currentVal.includes('.')){
-//         document.getElementById('display').value = currentVal + '.'
-//     }
-// }
+function inputZero(){
+    if(document.getElementById('display').value === '0'){
+        document.getElementById('display').value = document.getElementById('display').value + "";
+        currentDisplay =  document.getElementById('display').value; 
+    }else{
+        document.getElementById('display').value = document.getElementById('display').value + '0';
+        currentDisplay =  document.getElementById('display').value;
+        console.log("currentDisplay = " + currentDisplay); 
+    }
+};
+
+function decimal(){
+    if(!document.getElementById('display').value.includes('.')){
+        document.getElementById('display').value = document.getElementById('display').value + '.'
+    }
+}
